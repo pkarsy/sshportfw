ca11d8e05a17717236d4640c24368f23a911ffe8
Panagiotis Karagiannis <pkarsy@gmail.com>
2023-03-12 10:08:02 +0200

    Implement flags for file output

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ const (
 
 // global variables are bad but given how simple the program is, lets accept 2 of them
 var routineCount = safeCounter.New()
-var activeRoutines = safeCounter.New()
+var activeForwardings = safeCounter.New()
 
 type forwarding struct {
 	Service    string
@@ -41,6 +41,7 @@ type serverInfo struct {
 }
 
 // Stops the timer and drains the chan safely
+// TODO delete
 func stopTimer(t *time.Timer) {
 	t.Stop()
 	select {
@@ -52,17 +53,45 @@ func stopTimer(t *time.Timer) {
 func flagParse() {
 	var version bool
 	var syslogOutput bool
+	var output string
+	var lines bool
+	var printtime bool
 	flag.BoolVar(&version, "version", false, "prints current sshportfw version")
 	flag.BoolVar(&version, "v", false, "")
 	flag.BoolVar(&syslogOutput, "syslog", false, "redirects output to syslog")
 	flag.BoolVar(&syslogOutput, "s", false, "")
+	flag.StringVar(&output, "output", "", "Redirect output to file, only messages from ssh client are displayed to console. Use -o /dev/null for quiet operation")
+	flag.StringVar(&output, "o", "", "")
+	flag.BoolVar(&lines, "lines", false, "Print source code line numbers for debugging")
+	flag.BoolVar(&lines, "l", false, "")
+	flag.BoolVar(&printtime, "time", false, "Print date and time for every line of output (ignored on syslog output)")
+	flag.BoolVar(&printtime, "t", false, "")
 	flag.Parse()
+	currflags := 0
 	if version {
-		fmt.Println("sshportfw Version 0.6.0")
+		fmt.Println("sshportfw Version 0.6.2")
 		os.Exit(0)
 	}
+	if output != "" && syslogOutput {
+		log.Print("-s and -o flags cannot be enabled at the same time")
+		os.Exit(1)
+	}
+	if output != "" {
+		outfile, err := os.OpenFile(output, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.SetOutput(outfile)
+	}
+	if lines {
+		currflags = currflags | log.Lshortfile
+	}
+	if printtime && !syslogOutput {
+		// logger will print date and time
+		currflags = currflags | log.Ldate | log.Ltime
+	}
 	if syslogOutput {
-		log.Print("SYSLOG output")
+		log.SetFlags(0)
 		// Configure logger to write to the syslog
 		logwriter, err := syslog.New(syslog.LOG_NOTICE, Appname)
 		if err == nil {
@@ -71,6 +100,7 @@ func flagParse() {
 			log.Fatal("Cannot set syslog output")
 		}
 	}
+	log.SetFlags(currflags)
 }
 
 // Goroutine, takes a net.Conn spawns a ssh connection and bidirectionally transfers data
@@ -100,7 +130,7 @@ func sshInstance(localConn net.Conn, fw forwarding, host string) {
 	once := sync.Once{}
 	wg := sync.WaitGroup{} // We use it to wait for the goroutines
 	wg.Add(3)
-	activeRoutines.Inc()
+	activeForwardings.Inc()
 	go func() {
 		_, err := io.Copy(stdin, localConn)
 		localConn.Close() // we force the other io.Copy to terminate (reader)
@@ -148,7 +178,7 @@ func sshInstance(localConn net.Conn, fw forwarding, host string) {
 	once.Do(func() {
 		log.Printf("ssh forwarder #%d ends", id)
 	})
-	log.Printf("Active SSH forwardings remaining : %d", activeRoutines.Dec())
+	log.Printf("Active SSH forwardings remaining : %d", activeForwardings.Dec())
 }
 
 // listens to a local port and whan a local connection occurs
@@ -184,7 +214,6 @@ func localPortListen(fw forwarding, host string) {
 }
 
 func main() {
-	log.SetFlags(log.Lshortfile)
 	log.SetOutput(os.Stdout)
 	flagParse()
 	// The location of the config file
@@ -201,8 +230,8 @@ func main() {
 	{
 		lock := fslock.New("lock")
 		if err := lock.TryLock(); err != nil {
-			log.Print(err)
-			log.Print("Already running")
+			log.Print(err, "The program is probably already running")
+			//log.Print("Already running")
 			os.Exit(1)
 		}
 
